9a0b0f676a94ace94e59329f36b7d2c40af86de1
anam <anamshkhan5@gmail.com>
2024-01-01 12:45:06 +0000

    solution

---
+++ src/components/Loader/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const LoaderEl = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 767px) {
+  }
+`
