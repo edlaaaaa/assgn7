d88b3d4e1dd7b3792600b8125e43fafceade2cc5
anam <anamshkhan5@gmail.com>
2024-01-19 13:01:59 +0000

    solution

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -22,13 +22,13 @@ class Navbar extends Component {
         {value => {
           const {activeTheme} = value
           const color = activeTheme === 'light' ? '#000000' : '#ffffff'
-          const BgColor = activeTheme === 'light' ? '#616e7c' : '#475569'
+          const bgColor = activeTheme === 'light' ? '#616e7c' : '#475569'
 
           return (
             <DivContainer>
               <ListContainer>
                 <Link to="/">
-                  <ListItems color={`${color}`} bgColor={`${hoverBgColor}`}>
+                  <ListItems color={`${color}`} bgColor={`${bgColor}`}>
                     <span className="nav-icons">
                       <HiHome size={20} />
                     </span>{' '}
@@ -36,7 +36,7 @@ class Navbar extends Component {
                   </ListItems>
                 </Link>
                 <Link to="/trending">
-                  <ListItems color={`${color}`} bgColor={`${hoverBgColor}`}>
+                  <ListItems color={`${color}`} bgColor={`${bgColor}`}>
                     <span className="nav-icons">
                       <AiFillFire size={20} />
                     </span>{' '}
@@ -44,7 +44,7 @@ class Navbar extends Component {
                   </ListItems>
                 </Link>
                 <Link to="/gaming">
-                  <ListItems color={`${color}`} bgColor={`${hoverBgColor}`}>
+                  <ListItems color={`${color}`} bgColor={`${bgColor}`}>
                     <span className="nav-icons">
                       <SiYoutubegaming size={20} />
                     </span>{' '}
@@ -52,7 +52,7 @@ class Navbar extends Component {
                   </ListItems>
                 </Link>
                 <Link to="/saved-videos">
-                  <ListItems color={`${color}`} bgColor={`${hoverBgColor}`}>
+                  <ListItems color={`${color}`} bgColor={`${bgColor}`}>
                     <span className="nav-icons">
                       <MdPlaylistAdd size={20} />
                     </span>
